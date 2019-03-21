8da870264f0159b0f8b4e378548c16d1a1bf31b1
gogo <yunfenqiu@Yunfens-MacBook-Pro.local>
2019-03-21 23:50:07 +0800

    feat: update

--- packages/promise/index.js
+++
@@ -1,9 +0,0 @@
-console.log(Promise);
-new Promise(resolve => {
-  console.log(1);
-  resolve(3);
-  Promise.resolve().then(()=> console.log(4))
-}).then(num => {
-  console.log(num)
-});
-console.log(2);
